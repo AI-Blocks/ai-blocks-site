01f296e11183f0bb9e0bdf782175105a7386cf1b
Shao-Qian (SQ) Mah <shaoqianmah@gmail.com>
2021-04-16 18:53:39 +0800

    Update gen_thumbs.py

--- gen_thumbs.py
+++ gen_thumbs.py
@@ -1,7 +1,20 @@
+"""
+Generate thumbnails for every video in the 'vid' folder, taking the first frame.
+
+Setup, in Terminal:
+
+1. /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+2. brew install ffmpeg
+"""
+
 import os
 
-vid_folder = "public/static/vid"
-thumbs_folder = "public/static/vid/thumbnails"
+file_path = os.path.realpath(__file__)
+dir = os.path.split(file_path)[0]
+static = os.path.join("public", "static")
+
+vid_folder = os.path.join(dir, os.path.join(static, "vid"))
+thumbs_folder = os.path.join(dir, os.path.join(static, "thumbnails"))
 width = 400
 
 for f in os.listdir(vid_folder):
